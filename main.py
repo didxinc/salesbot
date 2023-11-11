daf0dd7f818415bcfef0df48cb616893d8ec7842
DIDX Inc <141808941+didxinc@users.noreply.github.com>
2023-11-11 16:53:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import openai
+import streamlit as st
+from PyPDF2 import PdfReader
+
+# Set up your OpenAI API credentials
+openai.api_key = st.secrets['OPENAI_API_KEY']
+
+# Load the PDF file
+pdf_file = open('didx2.pdf', 'rb')
+pdf_reader = PdfReader(pdf_file)
+pdf_text = ''
+for page in pdf_reader.pages:
+    pdf_text += page.extract_text()
+
+# Define a function to generate a response using OpenAI GPT-3
+def generate_response(prompt):
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return response['choices'][0]['message']['content']
+
+# Define the Streamlit app
+def app():
+    user_input = st.text_input("Your question about DIDX:")
+    if user_input:
+        prompt = f"You: {user_input}\nAssistant: {pdf_text}"
+        response = generate_response(prompt)
+        st.text_area("Assistant's response:", value=response)
+
+# Run the Streamlit app
+if __name__ == '__main__':
+    app()
